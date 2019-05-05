91a2caae4400cd638bb2c97a2984275096b911c3
Muhammed Kaplan <gokdeniz91@gmail.com>
2019-05-05 20:51:31 +0200

    Updated xampp to 7.3.4

--- automatic/xampp/tools/chocolateyinstall.ps1
+++ automatic/xampp/tools/chocolateyinstall.ps1
@@ -1,23 +1,23 @@
-﻿$ErrorActionPreference = 'Stop'; 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://www.apachefriends.org/xampp-files/7.2.5/xampp-win32-7.2.5-0-VC15-installer.exe' # download url, HTTPS preferred
+$url = 'https://www.apachefriends.org/xampp-files/7.3.4/xampp-windows-x64-7.3.4-0-VC15-installer.exe' # download url, HTTPS preferred
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
 
-  softwareName  = 'xampp*' 
-  checksum      = 'D7CEB1B7A8081141DD8CA6E69C305145150584E0'
-  checksumType  = 'sha1'
-  checksum64    = 'D7CEB1B7A8081141DD8CA6E69C305145150584E0'
-  checksumType64= 'sha1'
+    softwareName   = 'xampp*'
+    checksum       = '74a2f2386ddc4db33d47ad90823395dbb7b232d0'
+    checksumType   = 'sha1'
+    checksum64     = '74a2f2386ddc4db33d47ad90823395dbb7b232d0'
+    checksumType64 = 'sha1'
 
-  # MSI
-  silentArgs    = "--mode unattended"
-  validExitCodes= @(0, 3010, 1641)
+    # MSI
+    silentArgs     = "--mode unattended"
+    validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
